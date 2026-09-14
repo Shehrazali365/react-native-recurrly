88a43d4b75afff3398e09737763b8411bb2aa30c
react native recurrly <shehrazali365@gmail.com>
2026-09-14 12:51:19 +0500

    feat: project setup , nativewind , tab navigation

--- app/(tabs)/subscriptions.tsx
+++ app/(tabs)/subscriptions.tsx
@@ -1,13 +1,18 @@
-import { View, Text } from 'react-native'
+import { Text } from 'react-native'
 import React from 'react'
 import { Link } from 'expo-router'
+import { SafeAreaView as RNSafeAreaView } from "react-native-safe-area-context";
+import {styled} from "nativewind";
+
+const SafeAreaView = styled(RNSafeAreaView);
+ 
 
 const subscriptions = () => {
   return (
-    <View>
+    <SafeAreaView className="flex-1 bg-background p-5">
       <Text>subscriptions</Text>
       <Link href="/" className="mt-4 rounded text-white bg-primary p-4">Go Back</Link>
-    </View>
+    </SafeAreaView>
   )
 }
 
