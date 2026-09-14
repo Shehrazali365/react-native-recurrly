b50723cced57668da531b1b06a98581895957a44
react native recurrly <shehrazali365@gmail.com>
2026-09-14 15:14:33 +0500

    implement fonts

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -9,19 +9,12 @@ const SafeAreaView = styled(RNSafeAreaView);
 export default function App() {
   return (
     <SafeAreaView className="flex-1 bg-background p-5">
-      <Text className="text-xl font-bold text-blue-500">
+      <Text className="text-7xl font-sans-extrabold text-primary">
         Welcome to Nativewind Shehraz Ali! 
       </Text>
-      <Link href="/onboarding" className="mt-4 rounded text-white bg-primary p-4">Go to Onboarding</Link>
-      {/* <Link href="/(auth)/sign-in" className="mt-4 rounded text-white bg-primary p-4">Go to Sign in</Link> */}
-      {/* <Link href="/(auth)/sign-up" className="mt-4 rounded text-white bg-primary p-4">Go to Sign up</Link> */}
-
-      <Link href="/(tabs)/subscriptions" className="mt-4 rounded text-white bg-primary p-4">Spotify subscription</Link>
-
-      {/* <Link href={{
-        pathname : "/subscriptions/[id]",
-        params : {id : "claude"}
-      }} className="mt-4 rounded text-white bg-primary p-4">Claude max subscription</Link> */}
+      <Link href="/onboarding" className="mt-4 font-sans-bold rounded text-white bg-primary p-4">Go to Onboarding</Link>
+      {/* <Link href="/(auth)/sign-in" className="mt-4 font-sans-bold rounded text-white bg-primary p-4">Go to Sign in</Link> */}
+      {/* <Link href="/(auth)/sign-up" className="mt-4 font-sans-bold rounded text-white bg-primary p-4">Go to Sign up</Link> */}
     </SafeAreaView>
   );
 }
